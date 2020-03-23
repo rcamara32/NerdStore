089b74d6c599c8fc39be658dde353784a306ac38
Renan Faustino da Camara <renan.camara@live.com>
2020-03-23 00:05:43 +0000

    First entities.

---
+++ src/NerdStore.Catalog.Domain/Product.cs
@@ -0,0 +1,71 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Catalog.Domain
+{
+    public class Product : Entity, IAggregateRoot
+    {
+        public Guid CategoryId { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public bool IsActive { get; private set; }
+        public decimal Price { get; private set; }
+        public DateTime CreatedDate { get; private set; }
+        public string Image { get; private set; }
+        public int StockQuantity { get; private set; }
+
+        public virtual Category Category { get; set; }
+
+        public Product(string name, string description, bool isActive, decimal price,
+            Guid categoryId, DateTime createdDate, string image)
+        {
+            Name = name;
+            Description = description;
+            IsActive = isActive;
+            Price = price;
+            CategoryId = categoryId;
+            CreatedDate = createdDate;
+            Image = image;
+        }
+
+        public void Activate() => IsActive = true;
+        public void Deactivate() => IsActive = false;
+
+        public void ChangeCategory(Category category)
+        {
+            Category = category;
+            CategoryId = category.Id;
+        }
+
+        public void ChangeDescription(string description)
+        {
+            Description = description;
+        }
+
+        public void StockDebit(int quantity) 
+        {
+            if (quantity < 0)
+                quantity *= -1;
+
+            StockQuantity -= quantity;
+        }
+
+        public void StockReplenishment(int quantity)
+        {
+            StockQuantity += quantity;
+        }
+
+        public bool HasStock(int quantity) 
+        {
+            return StockQuantity >= quantity;
+        }
+
+        public void Valid()
+        { 
+            
+        }
+
+    }
+}
---
+++ src/NerdStore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var comparTo = obj as Entity;
+
+            if (ReferenceEquals(this, comparTo)) return true;
+            if (ReferenceEquals(null, comparTo)) return false;
+
+            return Id.Equals(comparTo.Id);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+            {
+                return false;
+            }
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 311) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id= {Id}]";
+        }
+
+    }
+}
